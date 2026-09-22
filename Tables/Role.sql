478a6855cb1e8bf71f170e97df35756fad9897a8
JonathanVB98 <jon_j125@hotmail.com>
2026-09-22 14:26:16 -0600

    feat: add user and role tables

---
+++ Tables/Role.sql
@@ -0,0 +1,54 @@
+﻿CREATE TABLE [dbo].[Role]
+(
+	[RoleId] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL, 
+    [Description] NCHAR(50) NOT NULL, 
+    [IsActive] BIT NOT NULL, 
+    [CreatedDate] DATETIME NOT NULL 
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Unique identifier of the role.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Role',
+    @level2type = N'COLUMN',
+    @level2name = 'RoleId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Name of the role.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Role',
+    @level2type = N'COLUMN',
+    @level2name = N'Name'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Detailed description of the role.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Role',
+    @level2type = N'COLUMN',
+    @level2name = N'Description'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Indicates whether the role is currently active.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Role',
+    @level2type = N'COLUMN',
+    @level2name = N'IsActive'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Date and time when the role was created.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Role',
+    @level2type = N'COLUMN',
+    @level2name = N'CreatedDate'
---
+++ Tables/User.sql
@@ -0,0 +1,116 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(100) NULL, 
+    [LastName] NCHAR(100) NULL, 
+    [UserName] NCHAR(50) NOT NULL, 
+    [Email] NCHAR(254) NOT NULL, 
+    [Password] NCHAR(255) NOT NULL, 
+    [Phone] NCHAR(20) NOT NULL, 
+    [RoleId] INT NOT NULL, 
+    [IsActive] BIT NOT NULL, 
+    [CreatedDate] DATETIME NOT NULL, 
+    [UpdatedDate] DATETIME NULL,
+    
+    CONSTRAINT [FK_User_Role] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Role] ([RoleId])
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Unique identifier for the user.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'UserId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'User''s first name.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'FirstName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'User''s last name.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'LastName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Username used to sign in.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'UserName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'User''s email address.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'Email'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Hashed password of the user.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'Password'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'User''s phone number.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'Phone'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Identifier of the role assigned to the user.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'RoleId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Indicates whether the user is active.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'IsActive'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Date and time when the user was created.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'CreatedDate'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Date and time when the user was last updated.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'UpdatedDate'
